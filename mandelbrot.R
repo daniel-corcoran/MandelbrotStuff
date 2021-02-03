4fd57821c92b92889f0d69b9239a9730d07201ef
daniel-corcoran <49919745+daniel-corcoran@users.noreply.github.com>
2021-02-03 16:11:08 -0500

    Update mandelbrot.R
    Version 2 - Sacrifice lower resolution for performance gainz, also implemented zoom feature (click on the plot to zoom 2x)

--- mandelbrot.R
+++ mandelbrot.R
@@ -1,11 +1,12 @@
 options(bitmapType='cairo')
+library("reshape2")
+library("ggplot2")
+
 mandelbrot <- function(cr, cx, depth){
   # Returns mandelbrot function for a complex coordinate
   x <- 0
   y <- 0
   iteration <- 0
-
-
   while(x*x + y*y <= 4 & iteration < depth )
     # Apply algorithm
   {
@@ -14,26 +15,61 @@ mandelbrot <- function(cr, cx, depth){
     x <- xtemp
     iteration <- iteration + 1
   }
-
- return(iteration)
+  return(iteration)
 }
-plot(1, type="n", xlab="", ylab="", xlim=c(-2.5, 1), ylim=c(-1, 1))
-x_pnt <- 500
-y_pnt <- 500
-depth <- 50
-gradient <- colorRampPalette(c("blue", "red", "green"))
-gradient <- gradient(depth)
-for(x in seq(x_pnt)){
-  print(100 * x / x_pnt)
-  for(y in seq(y_pnt)){
-    x_scale <- (3.5 * x / x_pnt) - 2.5
-    y_scale <- (2 * y / y_pnt) - 1
-    out <- mandelbrot(x_scale, y_scale, depth)
-    if(out == depth){
-      points(x_scale, y_scale, pch='.', col="black")
-    }
-    else{
-      points(x_scale, y_scale, pch='.', col=gradient[out])
-    }
+coordinates <- function(x_pnt, y_pnt, xlim.min, xlim.max, ylim.min, ylim.max){
+  mb <- data.frame(x<-seq(x_pnt), y<-seq(y_pnt))
+  names(mb) <- c("x", "y")
+  mb$n <- 0
+  mb.wide <- dcast(mb, x ~ y, value.var="n", sum, fill=0)
+  # FIXME: Where does this 500 correlate to? X or Y?
+  mb.melt <- melt(mb.wide, id.vars= "x", measure.vars=2:y_pnt, variable.name = "y")
+  mb.melt$x <- lapply(mb.melt$x, function(x) ((xlim.max - xlim.min) * x / x_pnt) + xlim.min)
+  mb.melt$y <- lapply(as.numeric(mb.melt$y), function(y) ((ylim.max - ylim.min) * y / y_pnt) + ylim.min)
+  return(mb.melt)
+}
+
+x_pnt <- 200
+y_pnt <- 200
+depth <- 15
+
+xlim.min <- -2.5
+xlim.max <- 1
+ylim.min <- -1
+ylim.max <- 1
+
+while(T){
+  gradient <- colorRampPalette(c("blue", "yellow"))
+  gradient <- gradient(depth)
+  c <- coordinates(x_pnt, y_pnt, xlim.min, xlim.max, ylim.min, ylim.max)
+  mb_helper <- function(x){
+    return(mandelbrot(x$x, x$y, depth))
   }
+  c$value <- apply(c, MARGIN = 1, FUN=mb_helper)
+  c$col<-lapply(c$value, function(v) ifelse(v == depth, "black", gradient[v]))
+  plot(1, type="n", xlab="", ylab="", 
+       xlim=c(xlim.min, xlim.max),
+       ylim=c(ylim.min, ylim.max))
+  cat("\nPlotting point vector")
+  plot(x=c$x, y=c$y, col=as.character(c$col), xlab = "", ylab="")
+  #ggplot(c, aes(as.numeric(x), as.numeric(y), color=c$col)) + geom_point()
+  cat("\nRender complete. Locating...")
+  g <- locator(1)
+  print(g)
+  # On click (zoom)
+  # Retain the same (center) but cut the X and Y range in half.
+  x_ctr <- g$x
+  y_ctr <- g$y
+  x_dst <- (xlim.max - xlim.min) / 4
+  y_dst <- (ylim.max - ylim.min) / 4
+  xlim.min <- x_ctr - x_dst
+  xlim.max <- x_ctr + x_dst
+  ylim.min <- y_ctr - y_dst
+  ylim.max <- y_ctr + y_dst
+  text(x_ctr, ylim.max, pos=1, labels="loading", col="white")
+  
+  depth <- depth + 8
+  print("New coordinates")
+  cat(sprintf("X limits: %f, %f \n", xlim.min, xlim.max))
+  cat(sprintf("Y limits: %f, %f ", ylim.min, ylim.max))
 }
